2c5b5f86a1f846319de27e0a90f63b139ca6549c
Paul Landázuri <landazuri.p@gmail.com>
2021-11-23 01:18:25 +0000

    feat: Parse arxiv entry

--- pkg/database/arxiv_eprint.go
+++ pkg/database/arxiv_eprint.go
@@ -1,20 +1,23 @@
 package database
 
+import "time"
+
 type ArxivEprint struct {
 	Id ID `db:"id"`
 
 	ArxivId string `db:"arxiv_id"`
 
-	Comment       string                 `db:"comment"`
-	Extra         map[string]interface{} `db:"extra"`
-	LatestVersion string                 `db:"latest_version"`
-	PdfLink       string                 `db:"pdf_link"`
+	Comment       *string                 `db:"comment"`
+	Extra         *map[string]interface{} `db:"extra"`
+	LatestVersion int                     `db:"latest_version"`
+	PdfLink       *string                 `db:"pdf_link"`
 
-	PublishedAt string `db:"published_at"`
-	UpdatedAt   string `db:"updated_at"`
+	PublishedAt time.Time `db:"published_at"`
+	UpdatedAt   time.Time `db:"updated_at"`
 
 	EprintId ID `db:"eprint_id"`
 
-	EPrint          *Eprint
-	ArxivCategories []*ArxivCategory
+	EPrint               *Eprint
+	PrimaryArxivCategory *ArxivCategory
+	OtherArxivCategories []*ArxivCategory
 }
--- pkg/scraper/arxiv/arxiv.go
+++ pkg/scraper/arxiv/arxiv.go
@@ -4,11 +4,14 @@ import (
 	"github.com/gocolly/colly/v2"
 	"github.com/papetier/scraper/pkg/database"
 	log "github.com/sirupsen/logrus"
+	"strconv"
 	"strings"
+	"time"
 )
 
 const (
 	arxivAbstractUrl = arxivBaseUrl + "abs/"
+	arxivPdfUrl      = arxivBaseUrl + "pdf/"
 	arxivBaseUrl     = "http://arxiv.org/"
 	arxivErrorTitle  = "Error"
 )
@@ -19,7 +22,7 @@ func SetupCollector(c *colly.Collector) {
 }
 
 func entryParser(e *colly.XMLElement) {
-	title := e.ChildText("//title")
+	title := e.ChildText("title")
 	if title == arxivErrorTitle {
 		handleErrorEntry(e)
 		return
@@ -36,7 +39,7 @@ func entryParser(e *colly.XMLElement) {
 	}
 
 	// parse id
-	id := e.ChildText("//id")
+	id := e.ChildText("id")
 	idParsingResult := strings.Split(id, arxivAbstractUrl)
 	if len(idParsingResult) < 2 {
 		log.Errorf("unexpected arxiv id format: %s", id)
@@ -46,21 +49,87 @@ func entryParser(e *colly.XMLElement) {
 		arxivEprint.ArxivId = arxivId
 	}
 
-	// TODO: parse doi
-	// TODO: parse abstract
-	// TODO: parse year
-	// TODO: parse journal_ref
+	// parse doi
+	doi := e.ChildText("arxiv:doi")
+	paper.Doi = doi
 
-	// TODO: parse published
-	// TODO: parse updated
-	// TODO: parse categories (with primary)
-	// TODO: parse extra
-	// TODO: parse latest_version
-	// TODO: parse pdf_link (if different from default)
+	// parse abstract
+	abstract := e.ChildText("summary")
+	paper.Abstract = abstract
+
+	// parse journal_ref
+	journalRef := e.ChildText("arxiv:journal_ref")
+	paper.JournalRef = journalRef
+
+	// TODO: parse year
+
+	// parse comment
+	comment := e.ChildText("arxiv:comment")
+	if comment != "" {
+		arxivEprint.Comment = &comment
+	}
+
+	// parse pdf_link (if different from default)
+	pdfLink := e.ChildAttr("link[@title='pdf']", "href")
+	if pdfLink != arxivPdfUrl+arxivEprint.ArxivId {
+		arxivEprint.PdfLink = &pdfLink
+	}
+
+	// parse published
+	publishedAtRaw := e.ChildText("published")
+	publishedAt, err := time.Parse(time.RFC3339, publishedAtRaw)
+	if err != nil {
+		log.Errorf("parsing published date for %s : %s", arxivEprint.ArxivId, err)
+	}
+	arxivEprint.PublishedAt = publishedAt
+
+	// parse updated
+	updatedAtRaw := e.ChildText("updated")
+	updatedAt, err := time.Parse(time.RFC3339, updatedAtRaw)
+	if err != nil {
+		log.Errorf("parsing updated date for %s : %s", arxivEprint.ArxivId, err)
+	}
+	arxivEprint.UpdatedAt = updatedAt
+
+	// parse latest_version
+	versionSplitResult := strings.Split(arxivEprint.ArxivId, "v")
+	if len(versionSplitResult) < 2 {
+		log.Errorf("unexpected arxiv id format for version parsing: %s", id)
+	} else {
+		latestVersionString := versionSplitResult[len(versionSplitResult)-1]
+		latestVersion, err := strconv.Atoi(latestVersionString)
+		if err != nil {
+			log.Errorf("formatting latest version (%s): %s", latestVersionString, err)
+		}
+		arxivEprint.LatestVersion = latestVersion
+	}
+
+	// parse categories (with primary) + extra categories
+	var otherArxivCategories []*database.ArxivCategory
+	var extraCategories []string
+	primaryCategoryCode := e.ChildAttr("arxiv:primary_category", "term")
+	categoryCodeList := e.ChildAttrs("category", "term")
+	for _, categoryCode := range categoryCodeList {
+		if arxivCategory, exists := categoriesByCodeMap[categoryCode]; exists {
+			if categoryCode == primaryCategoryCode {
+				arxivEprint.PrimaryArxivCategory = arxivCategory
+			} else {
+				otherArxivCategories = append(otherArxivCategories, arxivCategory)
+			}
+		} else {
+			extraCategories = append(extraCategories, categoryCode)
+		}
+	}
+	arxivEprint.OtherArxivCategories = otherArxivCategories
+
+	// parse extra (atm: only extra categories)
+	if len(extraCategories) > 0 {
+		arxivEprint.Extra = &map[string]interface{}{
+			"categories": extraCategories,
+		}
+	}
 
 	// TODO: save arxivEprint and co
-	log.Infof("new arxivEprint: %#v", arxivEprint)
-	log.Infof("new paper: %#v", paper)
 }
 
 func handleErrorEntry(e *colly.XMLElement) {
